9262d18ec8d1e45299823e3736135ff894258edb
zfreak1337 <kefiro888@gmail.com>
2022-01-16 17:08:43 +0600

    Add OneArgumentFactoryTests

---
+++ Calculator/Calculator/ArgumentsFactoryTests/OneArgument.cs
@@ -0,0 +1,20 @@
+using System;
+using NUnit.Framework;
+
+namespace Calculator.ArgumentsFactoryTests
+{
+    public class CalculatorOneArgument
+    {
+        [TestCase("Sin", typeof(Sin))]
+        [TestCase("Cos", typeof(Cos))]
+        [TestCase("Abs", typeof(Abs))]
+        [TestCase("Sqrt", typeof(Sqrt))]
+        [TestCase("Tg", typeof(Tg))]
+        [TestCase("Ctg", typeof(Ctg))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = OneArgumentFactory.CreateCalculator(name);
+            Assert.IsInstanceOf(type, calculator);
+        }
+    }
+}
