21d4021568ea36730a3e6e7d1447019e47486ab0
IHateBeingAlone <aroww200@gmail.com>
2022-01-16 17:04:30 +0600

    Create TwoArgumentsFactoryTests

---
+++ Calculator/Calculator/ArgumentsFactoryTests/TwoArguments.cs
@@ -0,0 +1,18 @@
+using System;
+using NUnit.Framework;
+
+namespace Calculator.ArgumentsFactoryTests
+{
+    public class CalculatorTwoArguments
+    {
+        [TestCase("Plus", typeof(Plus))]
+        [TestCase("Minus", typeof(Minus))]
+        [TestCase("Multiplication", typeof(Multiplication))]
+        [TestCase("Division", typeof(Division))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = TwoArgumentsFactory.CreateCalculator(name);
+            Assert.IsInstanceOf(type, calculator);
+        }
+    }
+}
